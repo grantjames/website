22f917f2c47b2269fd323a19562f6a91f5e20d67
Grant <grant@g-james.co.uk>
2017-10-09 19:41:25 +0100

    Analytics and footer icon styling

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,18 @@
     @include('partials.category_styles')
 </head>
 <body>
+    @if(Request::segment(1) != 'admin')
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-101743966-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    @endif
+
     <div class="container">
         <header class="header">
             <h2 class="header__breadcrumb">
