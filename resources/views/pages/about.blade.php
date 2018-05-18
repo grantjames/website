644cf234389826107025cffeb1a52c6835777f46
Grant <grant@g-james.co.uk>
2018-05-19 00:08:14 +0100

    Text on about page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,5 +3,15 @@
 @section('content')
 	<h1>About</h1>
 
-	<p>Coming soon...</p>
+	<p>Grant is a software developer in Birmingham.</p>
+
+	<p>Graduating from university in 2009 with 1st in Computer Science, he jumped straight into the industry coding websites and, later, mobile applications too.</p>
+
+	<p>He doesn't always write about himself in the third person, but when he does it's because he wants to sound like a robot, or to seem professional writing a bio like this. The human side of Grant likes <a href="/category/music">music</a> (both playing and listening to), <a href="/category/cooking">food</a> (cooking and eating) and spending time with his wife and son.</p>
+
+	<p>He is currently working as part of the Solution Development team at <a href="http://www.bcu.ac.uk" target="_new">Birmingham City University</a>.</p>
+
+	<p>You can find him on social media or by emailing <a href="&#x6d;&#x61;&#x69;&#x6c;&#116;&#x6f;&#x3a;&#x67;&#x72;&#x61;&#110;&#116;&#x40;&#103;&#45;&#106;&#x61;&#109;&#101;&#x73;&#x2e;&#99;&#111;&#x2e;&#117;&#x6b;">&#103;&#114;&#x61;&#110;&#116;&#64;&#x67;&#x2d;&#x6a;&#x61;&#x6d;&#101;&#115;&#46;&#x63;&#111;&#46;&#117;&#x6b;</a></p>
+
+	
 @endsection
