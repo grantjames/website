18e9509cbdc8f82bcde1a5ef0e54d80981c18f97
Grant <grant@g-james.co.uk>
2018-05-19 06:42:20 +0100

    Fix alignment for footer

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,6 +12,4 @@
 	<p>He is currently working as part of the Solution Development team at <a href="http://www.bcu.ac.uk" target="_new">Birmingham City University</a>.</p>
 
 	<p>You can find him on social media or by emailing <a href="&#x6d;&#x61;&#x69;&#x6c;&#116;&#x6f;&#x3a;&#x67;&#x72;&#x61;&#110;&#116;&#x40;&#103;&#45;&#106;&#x61;&#109;&#101;&#x73;&#x2e;&#99;&#111;&#x2e;&#117;&#x6b;">&#103;&#114;&#x61;&#110;&#116;&#64;&#x67;&#x2d;&#x6a;&#x61;&#x6d;&#101;&#115;&#46;&#x63;&#111;&#46;&#117;&#x6b;</a></p>
-
-	
 @endsection
