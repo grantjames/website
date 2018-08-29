8907c65b6e95da586f080d33b0466dc7454cb686
Grant <grant@g-james.co.uk>
2018-08-29 21:24:33 +0100

    Wording update on about page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<h1>About</h1>
 
-	<p>Grant is a software developer in Birmingham.</p>
+	<p>Grant is a software developer from Birmingham.</p>
 
 	<p>Graduating from university in 2009 with a 1st in Computer Science, he jumped straight into the industry coding websites and, later, mobile applications too.</p>
 
@@ -11,5 +11,5 @@
 
 	<p>He is currently working as part of the Solution Development team at <a href="http://www.bcu.ac.uk" target="_new">Birmingham City University</a>.</p>
 
-	<p>You can find him on social media or by emailing <a href="&#x6d;&#x61;&#x69;&#x6c;&#116;&#x6f;&#x3a;&#x67;&#x72;&#x61;&#110;&#116;&#x40;&#103;&#45;&#106;&#x61;&#109;&#101;&#x73;&#x2e;&#99;&#111;&#x2e;&#117;&#x6b;">&#103;&#114;&#x61;&#110;&#116;&#64;&#x67;&#x2d;&#x6a;&#x61;&#x6d;&#101;&#115;&#46;&#x63;&#111;&#46;&#117;&#x6b;</a></p>
+	<p>You can contact him by email on <a href="&#x6d;&#x61;&#x69;&#x6c;&#116;&#x6f;&#x3a;&#x67;&#x72;&#x61;&#110;&#116;&#x40;&#103;&#45;&#106;&#x61;&#109;&#101;&#x73;&#x2e;&#99;&#111;&#x2e;&#117;&#x6b;">&#103;&#114;&#x61;&#110;&#116;&#64;&#x67;&#x2d;&#x6a;&#x61;&#x6d;&#101;&#115;&#46;&#x63;&#111;&#46;&#117;&#x6b;</a></p>
 @endsection
