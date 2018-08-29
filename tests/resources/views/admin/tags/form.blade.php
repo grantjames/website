255f04015ed00051a0852d46a54ed069097bda67
Grant <grant@g-james.co.uk>
2018-08-30 00:26:32 +0100

    Removed duplicate files.

--- tests/resources/views/admin/tags/form.blade.php
+++
@@ -1,5 +0,0 @@
-{{ csrf_field() }}
-
-<label>
-	Tag name: <input type="text" name="name" value="{{ old('name', $tag->name ?? '') }}">
-</label>
--- tests/resources/views/pages/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-	<h1>About</h1>
-
-	<p>Coming soon...</p>
-@endsection
