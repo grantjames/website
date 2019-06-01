b2b93682c8c0bc917c9ef9275e97a82bcf0d3d54
Grant <grantjames@live.co.uk>
2019-06-01 07:24:34 +0100

    New analytics code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,14 +11,13 @@
 </head>
 <body class="{{ $theme . '-theme' }}">
     @if(Request::segment(1) != 'admin')
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141186556-1"></script>
         <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-            ga('create', 'UA-101743966-1', 'auto');
-            ga('send', 'pageview');
+            gtag('config', 'UA-141186556-1');
         </script>
     @endif
 
