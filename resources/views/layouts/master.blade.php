e41890d4f7cc1d798088f288a0ac8b47aae9d970
Grant <grantjames@live.co.uk>
2019-10-12 11:14:05 +0100

    Use mix helper to use versioned asset files

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
 
     @include('partials.category_styles')
 </head>
@@ -114,6 +114,6 @@
     </footer>
     
     @yield('scripts')
-    <script src="/js/app.js"></script>
+    <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
