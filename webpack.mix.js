74e33a287b620812b1434cc3187043dec51c58a5
Grant <grantjames@live.co.uk>
2019-10-24 22:54:43 +0100

    Only version asset files in production

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,6 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-//if (mix.inProduction()) {
+if (mix.inProduction()) {
    mix.version();
-//}
+}
